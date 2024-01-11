1c1136eb48db0483ebfe409c9020d29dbc4df45d
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2024-01-11 18:23:24 +0100

    Fix relURL usage

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ import * as bootstrap from 'js/bootstrap';
 window.addEventListener('DOMContentLoaded', (event) => {
     new PagefindUI({ 
         element: '#search',  
-        baseUrl: '{{ relURL "/" }}',
+        baseUrl: '{{ relURL "" }}',
         autofocus: true,
         showSubResults: true,
         showEmptyFilters: false
